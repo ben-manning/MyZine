e9d0832ee9f532160cb326c37b0e78f1cf3187fa
Ben Manning <bmanning@me.com>
2015-02-16 15:05:29 -0800

    add some features and testing

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,13 @@
 class FavoritesController < ApplicationController
+	def index	
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def destroy
+	end
 end
--- spec/routing/favorites_routing_spec.rb
+++ spec/routing/favorites_routing_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe FavoirtesController, type: :routing do
+RSpec.describe FavoritesController, type: :routing do
 	it 'routes to #new' do
 		expect(get('/users/1/favorites/new')).to route_to('favorites#new', user_id: '1')
 	end
@@ -9,7 +9,7 @@ RSpec.describe FavoirtesController, type: :routing do
 		expect(post('/users/1/favorites/')).to route_to('favorites#create', user_id: '1')
 	end
 
-	it 'routes to #destory' do
-		expect(delete('users/1/favorites/1')).to route_to('favorites#destory', user_id: '1', id: '1')
+	it 'routes to #destroy' do
+		expect(delete('users/1/favorites/1')).to route_to('favorites#destroy', user_id: '1', id: '1')
 	end
 end
